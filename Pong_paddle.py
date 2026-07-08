93fdd0420f8d1dc7f4e9364ee263a4614456a6d3
Nitheesh Reddy <nitheeshvengoti11@gmail.com>
2026-07-08 11:04:35 +0530

    Refactor paddle movement with constants and limits

--- Pong_paddle.py
+++ Pong_paddle.py
@@ -1,17 +1,23 @@
 from turtle import Turtle
 
+MOVE_DISTANCE = 20
+TOP_LIMIT = 250
+BOTTOM_LIMIT = -250
+
 class Paddle(Turtle):
-    def __init__(self,position):
+
+    def __init__(self, position):
         super().__init__()
         self.shape("square")
         self.color("white")
         self.penup()
-        self.goto(position)
         self.shapesize(stretch_wid=5, stretch_len=1)
+        self.goto(position)
 
     def go_up(self):
-        new_y = self.ycor() + 20
-        self.sety(new_y)
+        if self.ycor() < TOP_LIMIT:
+            self.sety(self.ycor() + MOVE_DISTANCE)
+
     def go_down(self):
-        new_y = self.ycor() - 20
-        self.sety(new_y)
+        if self.ycor() > BOTTOM_LIMIT:
+            self.sety(self.ycor() - MOVE_DISTANCE)
